3ac0166aab94243b5bd82ba19e93104e191cb699
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-03 19:23:02 +0200

    pocetak nove aplikacije motobazar

---
+++ MotobazarAPP/src/main/java/motobazar/MotobazarAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package motobazar;
+
+/**
+ *
+ * @author Kiki
+ */
+public class MotobazarAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
