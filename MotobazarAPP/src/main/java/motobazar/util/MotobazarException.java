59f4767bcb0c75bfe8618d6e3a791e03550351e9
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-05 19:34:23 +0200

    pravljenje alata

---
+++ MotobazarAPP/src/main/java/motobazar/util/MotobazarException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motobazar.util;
+
+
+public class MotobazarException extends Exception{
+    
+    private String poruka;
+    
+    public MotobazarException(String poruka){
+        super();
+        this.poruka = poruka;
+    }
+    public String getPoruka(){
+        return poruka;
+    }
+}
