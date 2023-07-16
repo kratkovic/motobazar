f12bba9d322ea567fd75d78dbd659ec930fec19f
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-16 12:26:19 +0200

    getteri i setteri za korisnika

--- MotobazarAPP/src/main/java/motobazar/model/Korisnik.java
+++ MotobazarAPP/src/main/java/motobazar/model/Korisnik.java
@@ -6,12 +6,59 @@ package motobazar.model;
 
 import jakarta.persistence.Entity;
 
-
 @Entity
-public class Korisnik extends Entitet{
-    
+public class Korisnik extends Entitet {
+
     private String ime;
     private String prezime;
     private String adresa;
     private String email;
+
+    public Korisnik() {
+        super();
+    }
+
+    public Korisnik(String ime, String prezime, String adresa, String email, int sifra) {
+        super(sifra);
+        this.ime = ime;
+        this.prezime = prezime;
+        this.adresa = adresa;
+        this.email = email;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+
 }
