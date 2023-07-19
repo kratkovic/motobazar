e8eb2e9d00cd6b97723caf622702d9e1ade6609b
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-19 17:52:00 +0200

    kategorija string

--- MotobazarAPP/src/main/java/motobazar/model/Kategorija.java
+++ MotobazarAPP/src/main/java/motobazar/model/Kategorija.java
@@ -8,5 +8,38 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Kategorija extends Entitet{
+    private String naziv;
+    private String opis;
+
+    public Kategorija() {
+        super();
+    }
+
+    public Kategorija (String naziv, String opis, int sifra) {
+        super(sifra);
+        this.naziv = naziv;
+        this.opis = opis;
+        
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+   
+    
+    
     
 }
