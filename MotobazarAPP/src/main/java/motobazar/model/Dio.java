15c298bec3d9c77a7ca54e4283d0819f99609ec2
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-22 17:26:47 +0200

    konstruktor

--- MotobazarAPP/src/main/java/motobazar/model/Dio.java
+++ MotobazarAPP/src/main/java/motobazar/model/Dio.java
@@ -21,6 +21,73 @@ public class Dio extends Entitet{
     public Dio() {
         super();
     }
+
+    public Dio(String naziv, String proizvodac, BigDecimal cijena, String kolicina, String opis, Korisnik korisnik, Kategorija kategorija, int sifra) {
+        super(sifra);
+        this.naziv = naziv;
+        this.proizvodac = proizvodac;
+        this.cijena = cijena;
+        this.kolicina = kolicina;
+        this.opis = opis;
+        this.korisnik = korisnik;
+        this.kategorija = kategorija;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getProizvodac() {
+        return proizvodac;
+    }
+
+    public void setProizvodac(String proizvodac) {
+        this.proizvodac = proizvodac;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+
+    public String getKolicina() {
+        return kolicina;
+    }
+
+    public void setKolicina(String kolicina) {
+        this.kolicina = kolicina;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public Korisnik getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(Korisnik korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public Kategorija getKategorija() {
+        return kategorija;
+    }
+
+    public void setKategorija(Kategorija kategorija) {
+        this.kategorija = kategorija;
+    }
     
     
     
