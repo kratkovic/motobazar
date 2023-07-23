1df8e2dfbe111fe7cb85702b3ed2b5fa73bc3b52
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-23 14:06:34 +0200

    pravljenje klase poruka

--- MotobazarAPP/src/main/java/motobazar/model/Kategorija.java
+++ MotobazarAPP/src/main/java/motobazar/model/Kategorija.java
@@ -4,39 +4,59 @@
  */
 package motobazar.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Kategorija extends Entitet {
 
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifra")
+    private int sifra;
+
+    @Column(name = "naziv")
     private String naziv;
+
+    @Column(name = "opis")
     private String opis;
+
 
     public Kategorija() {
         super();
     }
 
-    public Kategorija(String naziv, String opis, int sifra) {
-        super(sifra);
+    public Kategorija(String naziv, String opis) {
         this.naziv = naziv;
         this.opis = opis;
+    }
 
+   
+    @Override
+    public int getSifra() {
+        return sifra;
     }
 
     public String getNaziv() {
         return naziv;
     }
 
-    public void setNaziv(String naziv) {
-        this.naziv = naziv;
-    }
-
     public String getOpis() {
         return opis;
+    }
+
+    // Set metode
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
     }
 
     public void setOpis(String opis) {
         this.opis = opis;
     }
-
 }
+
+    
--- MotobazarAPP/src/main/java/motobazar/model/Poruka.java
+++ MotobazarAPP/src/main/java/motobazar/model/Poruka.java
@@ -4,9 +4,91 @@
  */
 package motobazar.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 
 @Entity
 public class Poruka extends Entitet{
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifra")
+    private int sifra;
+
+    @ManyToOne
+    @JoinColumn(name = "posiljatelj_sifra")
+    private Korisnik posiljatelj;
+
+    @ManyToOne
+    @JoinColumn(name = "primatelj_sifra")
+    private Korisnik primatelj;
+
+    @Column(name = "sadrzaj")
+    private String sadrzaj;
+
+    @Column(name = "datum_vrijeme")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datumVrijeme;
+
     
+    public Poruka() {
+        super();
+    }
+
+    public Poruka(Korisnik posiljatelj, Korisnik primatelj, String sadrzaj, Date datumVrijeme) {
+        this.posiljatelj = posiljatelj;
+        this.primatelj = primatelj;
+        this.sadrzaj = sadrzaj;
+        this.datumVrijeme = datumVrijeme;
+    }
+
+    // Get metode
+    @Override
+    public int getSifra() {
+        return sifra;
+    }
+
+    public Korisnik getPosiljatelj() {
+        return posiljatelj;
+    }
+
+    public Korisnik getPrimatelj() {
+        return primatelj;
+    }
+
+    public String getSadrzaj() {
+        return sadrzaj;
+    }
+
+    public Date getDatumVrijeme() {
+        return datumVrijeme;
+    }
+
+
+    public void setPosiljatelj(Korisnik posiljatelj) {
+        this.posiljatelj = posiljatelj;
+    }
+
+    public void setPrimatelj(Korisnik primatelj) {
+        this.primatelj = primatelj;
+    }
+
+    public void setSadrzaj(String sadrzaj) {
+        this.sadrzaj = sadrzaj;
+    }
+
+    public void setDatumVrijeme(Date datumVrijeme) {
+        this.datumVrijeme = datumVrijeme;
+    }
 }
+
+
