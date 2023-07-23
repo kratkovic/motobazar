dbc78e3bb9dc5065b5a3822bb55cb46091d0bf28
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-07-23 14:09:12 +0200

    klasa korisnik

--- MotobazarAPP/src/main/java/motobazar/model/Korisnik.java
+++ MotobazarAPP/src/main/java/motobazar/model/Korisnik.java
@@ -4,61 +4,93 @@
  */
 package motobazar.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Korisnik extends Entitet {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifra")
+    private int sifra;
+
+    @Column(name = "ime")
     private String ime;
+
+    @Column(name = "prezime")
     private String prezime;
+
+    @Column(name = "adresa")
     private String adresa;
+
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "lozinka")
+    private String lozinka;
+
+    // Konstruktori
     public Korisnik() {
-        super();
     }
 
-    public Korisnik(String ime, String prezime, String adresa, String email, int sifra) {
-        super(sifra);
+    public Korisnik(String ime, String prezime, String adresa, String email, String lozinka) {
         this.ime = ime;
         this.prezime = prezime;
         this.adresa = adresa;
         this.email = email;
+        this.lozinka = lozinka;
+    }
+
+   
+    @Override
+    public int getSifra() {
+        return sifra;
     }
 
     public String getIme() {
         return ime;
     }
 
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
     public String getPrezime() {
         return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
     }
 
     public String getAdresa() {
         return adresa;
     }
 
-    public void setAdresa(String adresa) {
-        this.adresa = adresa;
-    }
-
     public String getEmail() {
         return email;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    
-    
-
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
 }
+
+
