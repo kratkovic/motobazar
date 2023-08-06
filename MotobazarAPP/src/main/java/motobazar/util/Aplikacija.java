ebed7f09c7806610c11a06358a1302e67e0dad00
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-06 19:20:17 +0200

    klasa aplikacija

---
+++ MotobazarAPP/src/main/java/motobazar/util/Aplikacija.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motobazar.util;
+
+import motobazar.model.Operater;
+
+
+public class Aplikacija {
+    
+      public static final String NAZIV_APP="MotoBazar APP";
+      public static Operater OPERATER;
+    
+    
+}
