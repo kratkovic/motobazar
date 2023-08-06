419e84cd8202720de1c8855bcf4aa53cd08fff97
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-06 19:56:40 +0200

    klasa obrada

---
+++ MotobazarAPP/src/main/java/motobazar/controller/Obrada.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motobazar.controller;
+
+import java.util.List;
+import motobazar.model.Entitet;
+import motobazar.util.HibernateUtil;
+import motobazar.util.MotobazarException;
+import org.hibernate.Session;
+
+public abstract class Obrada<T extends Entitet> {
+
+    protected T entitet;
+    protected Session session;
+
+    public abstract List<T> read();
+
+    protected abstract void kontrolaUnos() throws MotobazarException;
+
+    protected abstract void kontrolaPromjena() throws MotobazarException;
+
+    protected abstract void kontrolaBrisanje() throws MotobazarException;
+
+    public Obrada() {
+        this.session = HibernateUtil.getSession();
+
+    }
+
+}
