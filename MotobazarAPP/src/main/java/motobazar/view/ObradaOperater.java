5588ff3c1a9dc90df8c42a749b4e722a319bbc75
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-15 17:48:31 +0200

    obrada operater

--- MotobazarAPP/src/main/java/motobazar/view/ObradaOperater.java
+++ MotobazarAPP/src/main/java/motobazar/view/ObradaOperater.java
@@ -4,10 +4,12 @@
  */
 package motobazar.view;
 
+import jakarta.persistence.NoResultException;
 import java.util.List;
 import motobazar.controller.Obrada;
 import motobazar.model.Operater;
 import motobazar.util.MotobazarException;
+import org.mindrot.jbcrypt.BCrypt;
 
 
 public class ObradaOperater extends Obrada<Operater>{
@@ -17,6 +19,48 @@ public class ObradaOperater extends Obrada<Operater>{
         return session.createQuery("from Operater", Operater.class).list();
     } 
 
+     public void unosAdminOperatera() {
+
+        Operater o = new Operater();
+        o.setIme("Kristijan");
+        o.setPrezime("Ratković");
+        o.setEmail("ratkovic.kristijan@gmail.com");
+        o.setLozinka(BCrypt.hashpw("Motobazar33",
+                BCrypt.gensalt()).toCharArray());
+
+        entitet = o;
+        try {
+            create();
+        } catch (MotobazarException ex) {
+
+        }
+    }
+         public Operater autoriziraj(String email, char[] lozinka) {
+        Operater o;
+        try {
+            o = session.createQuery("from Operater o where o.email=:email",
+                    Operater.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+
+        if (BCrypt.checkpw(new String(lozinka),
+                new String(o.getLozinka()))) {
+            return o;
+        }
+
+        return null;
+    }
+
+    
+    
+    
+     
+    
+    
+    
     @Override
     protected void kontrolaUnos() throws MotobazarException {
         
