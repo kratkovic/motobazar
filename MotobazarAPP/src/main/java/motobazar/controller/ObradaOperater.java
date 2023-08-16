4ad14c8b0a326f002044534fdac6de3f46620580
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-16 22:44:45 +0200

    prebacivanje obrade operatera

--- MotobazarAPP/src/main/java/motobazar/controller/ObradaOperater.java
+++ MotobazarAPP/src/main/java/motobazar/controller/ObradaOperater.java
@@ -2,11 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package motobazar.view;
+package motobazar.controller;
 
 import jakarta.persistence.NoResultException;
 import java.util.List;
-import motobazar.controller.Obrada;
 import motobazar.model.Operater;
 import motobazar.util.MotobazarException;
 import org.mindrot.jbcrypt.BCrypt;
