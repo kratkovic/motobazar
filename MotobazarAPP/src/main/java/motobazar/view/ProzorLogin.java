9424a2e7a06b58a5b77545394bb85d2b4f43f919
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-27 19:27:06 +0200

    prozor

--- MotobazarAPP/src/main/java/motobazar/view/ProzorLogin.java
+++ MotobazarAPP/src/main/java/motobazar/view/ProzorLogin.java
@@ -9,5 +9,9 @@ package motobazar.view;
  * @author Kiki
  */
 public class ProzorLogin {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
