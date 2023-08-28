eb22ba46d23d27c04e3304ac5018e81fc5acc9bb
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-08-28 19:07:15 +0200

    obradadio

--- MotobazarAPP/src/main/java/motobazar/controller/ObradaDio.java
+++ MotobazarAPP/src/main/java/motobazar/controller/ObradaDio.java
@@ -4,7 +4,32 @@
  */
 package motobazar.controller;
 
+import java.util.List;
+import motobazar.model.Dio;
+import motobazar.util.MotobazarException;
 
-public class ObradaDio {
-    
+
+public class ObradaDio extends Obrada<Dio> {
+
+    @Override
+    public List<Dio> read() {
+       
+        return session.createQuery("from Dio", Dio.class).list();
+       
+    }
+
+    @Override
+    protected void kontrolaUnos() throws MotobazarException {
+      
+    }
+
+    @Override
+    protected void kontrolaPromjena() throws MotobazarException {
+        
+    }
+
+    @Override
+    protected void kontrolaBrisanje() throws MotobazarException {
+       
+    }
 }
