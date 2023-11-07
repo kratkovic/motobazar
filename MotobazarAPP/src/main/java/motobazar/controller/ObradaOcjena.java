ab0eaaca64d136cc542b2036df26b1cb37e0c1d2
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-11-07 20:39:20 +0100

    Update ObradaOcjena.java

--- MotobazarAPP/src/main/java/motobazar/controller/ObradaOcjena.java
+++ MotobazarAPP/src/main/java/motobazar/controller/ObradaOcjena.java
@@ -34,19 +34,19 @@ public class ObradaOcjena extends Obrada<Ocjena>{
     }
 
     private void kontrolaNazivNull() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     private void kontrolaNazivNijeBroj() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
 
     private void kontrolaNazivMinimalnaDuzina() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     private void kontrolaNazivMaksimalnaDuzina() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+      
     }
     
 }
