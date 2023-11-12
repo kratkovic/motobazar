57a1d53d804deccd688580eea275fac1a36cec81
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-11-12 16:59:32 +0100

    Update ObradaKategorija.java

--- MotobazarAPP/src/main/java/motobazar/controller/ObradaKategorija.java
+++ MotobazarAPP/src/main/java/motobazar/controller/ObradaKategorija.java
@@ -52,8 +52,7 @@ public class ObradaKategorija extends Obrada<Kategorija> {
         }
 
     }
-
-    private void kontrolaNazivMinimalnaDuzina() throws MotobazarException {
+     private void kontrolaNazivMinimalnaDuzina() throws MotobazarException {
         if (entitet.getNaziv().trim().length() < 3) {
             throw new MotobazarException("Naziv kategorije mora imati minimalno 3 znaka");
         }
