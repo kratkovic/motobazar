a8dfcd609f8595d291d78d5a0611ec0c1429dc25
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2025-03-18 21:53:01 +0100

    update za klasu poruka

--- MotobazarAPP/src/main/java/motobazar/util/MotobazarException.java
+++ MotobazarAPP/src/main/java/motobazar/util/MotobazarException.java
@@ -2,22 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
 package motobazar.util;
 
-public class MotobazarException extends Exception{
+public class MotobazarException extends Exception {
     
     private String poruka;
     
-    public MotobazarException(String poruka){
+    // Konstruktor koji prima samo poruku
+    public MotobazarException(String poruka) {
         super();
         this.poruka = poruka;
     }
 
-    public MotobazarException(String došlo_je_do_pogreške_pri_dohvatu_dijela_i, Exception e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    // Konstruktor koji prima poruku i originalnu iznimku
+    public MotobazarException(String poruka, Exception e) {
+        super(e);  // Prosljeđujem originalnu iznimku roditeljskoj klasi
+        this.poruka = poruka;  // Pohranjujem poruku
     }
     
-    public String getPoruka(){
+    // Getter za poruku
+    public String getPoruka() {
         return poruka;
     }
 }
